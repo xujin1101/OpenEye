587cf1b4f08243448262edb929eb7c3d4d284221
dllo <xujin6011@163.com>
2016-08-31 16:37:13 +0800

    select

--- Eyepetizer/app/src/main/java/com/eye/eyepetizer/select/SelectFragment.java
+++ Eyepetizer/app/src/main/java/com/eye/eyepetizer/select/SelectFragment.java
@@ -1,5 +1,7 @@
 package com.eye.eyepetizer.select;
 
+import android.widget.ListView;
+
 import com.eye.eyepetizer.R;
 import com.eye.eyepetizer.base.BaseFragment;
 
@@ -7,6 +9,7 @@ import com.eye.eyepetizer.base.BaseFragment;
  * 精选Fragment
  */
 public class SelectFragment extends BaseFragment {
+    private ListView mListView;
     @Override
     protected int getLayout() {
         return R.layout.fragment_select;
@@ -14,7 +17,7 @@ public class SelectFragment extends BaseFragment {
 
     @Override
     protected void initView() {
-
+        mListView = findView(R.id.select_lv);
     }
 
     @Override
