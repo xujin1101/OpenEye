04e57b756ff20eb6dd19084b0dc16c8bccceb61c
yjssssss <dllo@yangjiashuang30.local>
2016-08-31 11:22:09 +0800

    完成发现页面的主界面的布局

---
+++ Eyepetizer/app/src/main/java/com/eye/eyepetizer/discover/Bean.java
@@ -0,0 +1,260 @@
+package com.eye.eyepetizer.discover;
+
+import java.util.List;
+
+/**
+ * 
+ */
+public class Bean {
+
+    /**
+     * itemList : [{"type":"horizontalScrollCard","data":{"dataType":"HorizontalScrollCard","itemList":[{"type":"banner1","data":{"dataType":"Banner","id":106,"title":"","description":"","image":"http://img.kaiyanapp.com/b8c50281716659783895c97c7de77bb1.jpeg","actionUrl":"0","adTrack":null,"shade":false}},{"type":"banner1","data":{"dataType":"Banner","id":14,"title":"","description":"","image":"http://img.kaiyanapp.com/2c892eeefb675b22636ce6f71d2ec76c.png","actionUrl":"eyepetizer://recommend/","adTrack":null,"shade":false}}],"count":2}},{"type":"squareCard","data":{"dataType":"SquareCard","id":-1,"title":"","image":"http://img.wdjimg.com/image/video/46360c61d96fef74a776b3db1e221db7_0_0.jpeg","actionUrl":"eyepetizer://ranklist/","shade":false,"adTrack":null}}]
+     * count : 16
+     * total : 0
+     * nextPageUrl : null
+     */
+
+    private int count;
+    private int total;
+    private Object nextPageUrl;
+    /**
+     * type : horizontalScrollCard
+     * data : {"dataType":"HorizontalScrollCard","itemList":[{"type":"banner1","data":{"dataType":"Banner","id":106,"title":"","description":"","image":"http://img.kaiyanapp.com/b8c50281716659783895c97c7de77bb1.jpeg","actionUrl":"0","adTrack":null,"shade":false}},{"type":"banner1","data":{"dataType":"Banner","id":14,"title":"","description":"","image":"http://img.kaiyanapp.com/2c892eeefb675b22636ce6f71d2ec76c.png","actionUrl":"eyepetizer://recommend/","adTrack":null,"shade":false}}],"count":2}
+     */
+
+    private List<ItemListBean> itemList;
+
+
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public Object getNextPageUrl() {
+        return nextPageUrl;
+    }
+
+    public void setNextPageUrl(Object nextPageUrl) {
+        this.nextPageUrl = nextPageUrl;
+    }
+
+    public List<ItemListBean> getItemList() {
+        return itemList;
+    }
+
+    public void setItemList(List<ItemListBean> itemList) {
+        this.itemList = itemList;
+    }
+
+    public static class ItemListBean {
+        private String type;
+        /**
+         * dataType : HorizontalScrollCard
+         * itemList : [{"type":"banner1","data":{"dataType":"Banner","id":106,"title":"","description":"","image":"http://img.kaiyanapp.com/b8c50281716659783895c97c7de77bb1.jpeg","actionUrl":"0","adTrack":null,"shade":false}},{"type":"banner1","data":{"dataType":"Banner","id":14,"title":"","description":"","image":"http://img.kaiyanapp.com/2c892eeefb675b22636ce6f71d2ec76c.png","actionUrl":"eyepetizer://recommend/","adTrack":null,"shade":false}}]
+         * count : 2
+         */
+
+        private DataBean data;
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public DataBean getData() {
+            return data;
+        }
+
+        public void setData(DataBean data) {
+            this.data = data;
+        }
+
+        public static class DataBean {
+            private String dataType;
+            private int count;
+            private int id;
+            private String title;
+            private String image , actionUrl;
+
+            public int getId() {
+                return id;
+            }
+
+            public void setId(int id) {
+                this.id = id;
+            }
+
+            public String getTitle() {
+                return title;
+            }
+
+            public void setTitle(String title) {
+                this.title = title;
+            }
+
+            public String getImage() {
+                return image;
+            }
+
+            public void setImage(String image) {
+                this.image = image;
+            }
+
+            public String getActionUrl() {
+                return actionUrl;
+            }
+
+            public void setActionUrl(String actionUrl) {
+                this.actionUrl = actionUrl;
+            }
+
+            private List<ItemListBean1> itemList;
+
+            public String getDataType() {
+                return dataType;
+            }
+
+            public void setDataType(String dataType) {
+                this.dataType = dataType;
+            }
+
+            public int getCount() {
+                return count;
+            }
+
+            public void setCount(int count) {
+                this.count = count;
+            }
+
+            public List<ItemListBean1> getItemList() {
+                return itemList;
+            }
+
+            public void setItemList(List<ItemListBean1> itemList) {
+                this.itemList = itemList;
+            }
+
+            public static class ItemListBean1 {
+                private String type;
+                /**
+                 * dataType : Banner
+                 * id : 106
+                 * title :
+                 * description :
+                 * image : http://img.kaiyanapp.com/b8c50281716659783895c97c7de77bb1.jpeg
+                 * actionUrl : 0
+                 * adTrack : null
+                 * shade : false
+                 */
+
+                private DataBean1 data;
+
+                public String getType() {
+                    return type;
+                }
+
+                public void setType(String type) {
+                    this.type = type;
+                }
+
+                public DataBean1 getData() {
+                    return data;
+                }
+
+                public void setData(DataBean1 data) {
+                    this.data = data;
+                }
+
+                public static class DataBean1 {
+                    private String dataType;
+                    private int id;
+                    private String title;
+                    private String description;
+                    private String image;
+                    private String actionUrl;
+                    private Object adTrack;
+                    private boolean shade;
+
+                    public String getDataType() {
+                        return dataType;
+                    }
+
+                    public void setDataType(String dataType) {
+                        this.dataType = dataType;
+                    }
+
+                    public int getId() {
+                        return id;
+                    }
+
+                    public void setId(int id) {
+                        this.id = id;
+                    }
+
+                    public String getTitle() {
+                        return title;
+                    }
+
+                    public void setTitle(String title) {
+                        this.title = title;
+                    }
+
+                    public String getDescription() {
+                        return description;
+                    }
+
+                    public void setDescription(String description) {
+                        this.description = description;
+                    }
+
+                    public String getImage() {
+                        return image;
+                    }
+
+                    public void setImage(String image) {
+                        this.image = image;
+                    }
+
+                    public String getActionUrl() {
+                        return actionUrl;
+                    }
+
+                    public void setActionUrl(String actionUrl) {
+                        this.actionUrl = actionUrl;
+                    }
+
+                    public Object getAdTrack() {
+                        return adTrack;
+                    }
+
+                    public void setAdTrack(Object adTrack) {
+                        this.adTrack = adTrack;
+                    }
+
+                    public boolean isShade() {
+                        return shade;
+                    }
+
+                    public void setShade(boolean shade) {
+                        this.shade = shade;
+                    }
+                }
+            }
+        }
+    }
+}
--- Eyepetizer/app/src/main/java/com/eye/eyepetizer/discover/DiscoverFragment.java
+++ Eyepetizer/app/src/main/java/com/eye/eyepetizer/discover/DiscoverFragment.java
@@ -1,14 +1,36 @@
 package com.eye.eyepetizer.discover;
 
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 import com.eye.eyepetizer.R;
 import com.eye.eyepetizer.base.BaseFragment;
+import com.eye.eyepetizer.okHttp.NetTool;
+import com.eye.eyepetizer.okHttp.onHttpCallBack;
+import com.youth.banner.Banner;
+import com.youth.banner.Banner.OnBannerClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 发现Fragment
  */
-public class DiscoverFragment extends BaseFragment{
+public class DiscoverFragment extends BaseFragment implements OnClickListener{
+    private ImageView imgMatch,imgFirst,imgSecond;
+    private Banner mBanner;
+    private List<String> bannerUrl;
+    private GridView mGlideView;
+    private DiscoverAdapter mDiscoverAdapter;
 
 
+    String url = "http://baobab.wandoujia.com/api/v3/discovery?udid=cd1ee9c5b44e4f9487a505a4fe31ddcb07441cc8&vc=121&vn=2.3.5&deviceModel";
     @Override
     protected int getLayout() {
         return R.layout.fragment_discover;
@@ -16,11 +38,83 @@ public class DiscoverFragment extends BaseFragment{
 
     @Override
     protected void initView() {
+        imgMatch = findView(R.id.img_match);
+        imgFirst = findView(R.id.img_first);
+        imgSecond = findView(R.id.img_second);
+        mBanner = findView(R.id.banner);
+        mGlideView = findView(R.id.grid_view);
 
+        imgMatch.setOnClickListener(this);
+        imgFirst.setOnClickListener(this);
+        imgSecond.setOnClickListener(this);
     }
 
     @Override
     protected void initDate() {
+        bannerUrl = new ArrayList<>();
+        mDiscoverAdapter = new DiscoverAdapter(context);
+        NetTool.getInstance().startRequest(url, DiscoverBean.class, new onHttpCallBack<DiscoverBean>() {
+            @Override
+            public void onSuccess(DiscoverBean response) {
+                for (int i = 0; i < response.getItemList().get(0).getData().getItemList().size(); i++) {
+                    bannerUrl.add(response.getItemList().get(0).getData().getItemList().get(i).getData().getImage());
+                    //设置间隔
+                    mBanner.setDelayTime(3000);
+                    //添加图片
+                    mBanner.setImages(bannerUrl);
+                    //banner加点
+                    mBanner.setBannerStyle(Banner.CIRCLE_INDICATOR);
+                    //点居中
+                    mBanner.setIndicatorGravity(Banner.CENTER);
+                    mBanner.setOnBannerClickListener(new OnBannerClickListener() {
+                        @Override
+                        public void OnBannerClick(View view, int position) {
+                            Toast.makeText(context, "点击了第"+position+"张", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
 
+
+                String FirstImg = response.getItemList().get(1).getData().getImage();
+                String SecondImg = response.getItemList().get(2).getData().getImage();
+                String Match = response.getItemList().get(3).getData().getImage();
+                Glide.with(getContext()).load(Match).into(imgMatch);
+                Glide.with(getContext()).load(FirstImg).into(imgFirst);
+                Glide.with(getContext()).load(SecondImg).into(imgSecond);
+
+                mDiscoverAdapter.setBean(response);
+                mGlideView.setAdapter(mDiscoverAdapter);
+
+                mGlideView.setOnItemClickListener(new OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Toast.makeText(context, "点击了第" + position+"个", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.img_first:
+                Toast.makeText(context, "点击了最受欢迎", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.img_second:
+                Toast.makeText(context, "点击了热门专题", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.img_match:
+                Toast.makeText(context, "点击了360°", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
     }
 }
